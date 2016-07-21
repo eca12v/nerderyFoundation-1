b8adedcf29046645ee6f01b6ef44a0c04e553b38
gitthew <mcrawford37@gmail.com>
2016-07-21 16:46:11 -0500

    handleshake

--- routes/groups.js
+++ routes/groups.js
@@ -8,16 +8,23 @@ var router = express.Router();
 router.use(bodyParser.json());
 router.use(bodyParser.urlencoded({ extended: true }));
 
-
 router.post('/createGroup', function (req, res) {
   console.log(req.body);
-  var groupToAdd = {
+
+  var newGroup = new group({
     groupName: req.body.groupName,
     groupURL: req.body.groupURL,
-    contactName: req.body.contactName
-  };
-  var newGroup = group(groupToAdd);
-  newGroup.save();
+    groupContact: req.body.contactName
+  });
+
+  newGroup.save(function(err) {
+    if(err) {
+      console.log(err);
+      res.sendStatus(500);
+    } else {
+      res.json(newSound);
+    }
+  });
 });//end of post createGroup
 
 module.exports = router;
