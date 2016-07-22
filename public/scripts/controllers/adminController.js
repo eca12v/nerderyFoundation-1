23f4d4eba562fc9571a19f9691816c95bf7bfaae
anna baker <annakatherinebaker@gmail.com>
2016-07-22 16:29:48 -0500

    routes started

---
+++ public/scripts/controllers/adminController.js
@@ -0,0 +1,8 @@
+myApp.controller( 'AdminController', ['$scope', '$http', '$location', '$rootScope', function( $scope, $http, $location, $rootScope ){
+console.log( 'loaded AdminController');
+
+
+
+
+
+}]); //end adminController
---
+++ public/scripts/controllers/homeController.js
@@ -0,0 +1,9 @@
+myApp.controller( 'HomeController', ['factory', '$scope', '$http', '$location', '$rootScope', 
+function( $scope, $http, $location, $rootScope ){
+console.log( 'loaded homeController');
+
+
+
+
+
+}]); //end homeController
