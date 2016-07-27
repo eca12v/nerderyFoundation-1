704f54c5654e62526d57a3a03c02a2efc1efadfc
anna baker <annakatherinebaker@gmail.com>
2016-07-27 16:25:03 -0500

    testing display stuff

--- public/scripts/controllers/displayCtrl.js
+++ public/scripts/controllers/displayCtrl.js
@@ -1,6 +1,11 @@
-myApp.controller( 'DisplayCtrl', ['$scope', '$http', '$location', '$rootScope', function( $scope, $http, $location, $rootScope ){
+myApp.controller( 'DisplayCtrl', ['groupFactory', '$scope', '$http', '$routeParams', '$location', '$rootScope', function( $scope, $http, $location, groupFactory, $rootScope, $routeParams ){
 console.log( 'loaded DisplayController');
 
+groupFactory.getGroup($routeParams.groupName).then(function(response) {
+  $scope.group = response.data;
+  console.log($scope.group);
+
+});
 
 
 }]); //end displayController
