e30a8cfa40d86b4040509d036ea79c023ea89523
anna baker <annakatherinebaker@gmail.com>
2016-07-27 11:14:28 -0500

    chips not erroring

--- public/scripts/controllers/groupLeaderController.js
+++ public/scripts/controllers/groupLeaderController.js
@@ -1,6 +1,6 @@
 console.log('group leader cont has arrived');
 
-myApp.controller( 'GroupLeaderController',  [ 'groupFactory', '$scope', '$http', '$location', '$rootScope',  function( groupFactory, $scope,  $http, $location, $authProvider, $rootScope ){
+myApp.controller( 'GroupLeaderController', [ 'groupFactory', '$scope', '$http', '$location', '$rootScope',  function( groupFactory, $scope,  $http, $location, $authProvider, $rootScope ){
 
 console.log( 'loaded GroupLeaderController');
 // $mdIconProvider.icon('md-close', 'img/icons/ic_close_24px.svg', 24);
@@ -23,21 +23,10 @@ $scope.meetingSizes = [
   "100-500"
 ];
 
-function DemoCtrl () {
-    var self = this;
-
-    self.readonly = false;
-
-    // Lists of fruit names and Vegetable objects
-    self.techNames = ['Angular', 'jQuery', 'JavaScript'];
-    self.roTechNames = angular.copy(self.techNames);
-    self.editableTechNames = angular.copy(self.techNames);
-
-    self.tags = [];
-    self.techObjs = [
-      {
+$scope.technologies = [
+    {
         'name' : 'jQuery',
-      },
+    },
       {
         'name' : 'Java',
       },
@@ -46,13 +35,6 @@ function DemoCtrl () {
       }
     ];
 
-    self.newTech = function(chip) {
-      return {
-        name: chip,
-        type: 'unknown'
-      };
-    };
-  }
 
 $scope.status = '';
 
@@ -108,22 +90,5 @@ $scope.submit = function(){
 );
 
 };//end of submit
-$scope.user = {
-      title: 'Developer',
-      email: 'ipsum@lorem.com',
-      firstName: '',
-      lastName: '',
-      company: 'Google',
-      address: '1600 Amphitheatre Pkwy',
-      city: 'Mountain View',
-      state: 'CA',
-      biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
-      postalCode: '94043'
-    };
-    $scope.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
-    'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
-    'WY').split(' ').map(function(state) {
-        return {abbrev: state};
-      });
 
 }]); //end adminController
