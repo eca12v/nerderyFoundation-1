add16afbbac1af3dd5551fdb857f86ebf740875d
gitthew <mcrawford37@gmail.com>
2016-07-27 15:32:35 -0500

    implement chips in group create form

--- public/scripts/controllers/groupLeaderController.js
+++ public/scripts/controllers/groupLeaderController.js
@@ -7,6 +7,79 @@ console.log( 'loaded GroupLeaderController');
 //////---------
 //////---------
 
+var self = this;
+  self.readonly = false;
+  self.selectedItem = null;
+  self.searchText = null;
+  self.querySearch = querySearch;
+  self.vegetables = loadVegetables();
+  self.selectedVegetables = [];
+  self.roSelectedVegetables = angular.copy(self.selectedVegetables);
+  self.numberChips = [];
+  self.numberChips2 = [];
+  self.numberBuffer = '';
+  self.autocompleteDemoRequireMatch = true;
+  self.transformChip = transformChip;
+  self.fruitNames = ['Beer', 'Pizza'];
+  self.roFruitNames = angular.copy(self.fruitNames);
+  /**
+   * Return the proper object when the append is called.
+   */
+  function transformChip(chip) {
+    // If it is an object, it's already a known chip
+    if (angular.isObject(chip)) {
+      return chip;
+    }
+    // Otherwise, create a new one
+    return { name: chip, type: 'new' };
+  }
+  /**
+   * Search for vegetables.
+   */
+  function querySearch (query) {
+    var results = query ? self.vegetables.filter(createFilterFor(query)) : [];
+    return results;
+  }
+  /**
+   * Create filter function for a query string
+   */
+  function createFilterFor(query) {
+    var lowercaseQuery = angular.lowercase(query);
+    return function filterFn(vegetable) {
+      return (vegetable._lowername.indexOf(lowercaseQuery) === 0) ||
+          (vegetable._lowertype.indexOf(lowercaseQuery) === 0);
+    };
+  }
+  function loadVegetables() {
+    var veggies = [
+      {
+        'name': 'AngularJS',
+        'type': 'Javascript'
+      },
+      {
+        'name': 'Node.js',
+        'type': 'Javascript'
+      },
+      {
+        'name': '.NET',
+        'type': 'Web Technology'
+      },
+      {
+        'name': 'SASS',
+        'type': 'CSS'
+      },
+      {
+        'name': 'Blah',
+        'type': 'Blah'
+      }
+    ];
+    return veggies.map(function (veg) {
+      veg._lowername = veg.name.toLowerCase();
+      veg._lowertype = veg.type.toLowerCase();
+      return veg;
+    });
+  }
+
 //create array to put new groups into
 $scope.groups = [];
 $scope.membershipSizes = [
@@ -60,7 +133,7 @@ $scope.submit = function(){
     location: $scope.location,
     activities:$scope.activities,
     technologies: $scope.technologies,
-    tags: $scope.tags,
+    tags: self.roFruitNames,
     freqOfMeeting: $scope.freqOfMeeting,
     sizeOfMeeting: $scope.sizeOfMeeting,
     affiliations: $scope.affiliations,
