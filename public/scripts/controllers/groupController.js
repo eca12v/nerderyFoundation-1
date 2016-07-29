1e14f34be35561e027ea257cb864773c63982647
anna baker <annakatherinebaker@gmail.com>
2016-07-29 12:03:10 -0500

    merging

--- public/scripts/controllers/groupController.js
+++ public/scripts/controllers/groupController.js
@@ -5,4 +5,26 @@ myApp.controller( 'GroupController', ['$scope', '$http', '$location', '$rootScop
     console.log($scope.group);
 	});
 
+  $scope.group = {
+     name: 'name',
+     tel: '123-45-67',
+     number: 29,
+     range: 10,
+     url: 'http://example.com',
+     search: 'blabla',
+     color: '#6a4415',
+     date: null,
+     time: '12:30',
+     datetime: null,
+     month: null,
+     week: null
+   };
+  //  console.log( $scope.group.name );
+  $scope.membershipSizes = [
+          "0-25",
+          "25-50",
+          "50-100",
+          "100-500"
+      ];
+
 }]); //end controller
