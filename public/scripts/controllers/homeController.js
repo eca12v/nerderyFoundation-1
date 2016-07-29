81de3a57709c3c86506d3148fd685f18b847d9fc
gitthew <mcrawford37@gmail.com>
2016-07-29 15:46:39 -0500

    build separate tech filter style

--- public/scripts/controllers/homeController.js
+++ public/scripts/controllers/homeController.js
@@ -2,16 +2,33 @@ myApp.controller( 'HomeController', ['$scope', '$http', '$location', 'groupFacto
 function( $scope, $http, $location, groupFactory){
 console.log( 'loaded homeController');
 
+$scope.selectedTech = '';
 $scope.techStr = '';
+$scope.overallTechStr = '';
+
+$scope.changeOverallTech = function(tech) {
+  console.log('overallchangeTech hit');
+  console.log(tech);
+  $scope.selectedTech = tech;
+  // console.log(tech.Skills);
+  if ($scope.techStr) {
+    $scope.techStr = '';
+    $scope.techStr += tech + ' ';
+  } else {
+    $scope.techStr += tech + ' ';
+  }
+};
 
 $scope.changeTechStr =  function(tech) {
+  console.log('changeTech hit');
   console.log(tech);
-  var sub = $scope.techStr.indexOf(tech.Technology);
+  // console.log(tech.Skills);
+  var sub = $scope.techStr.indexOf(tech);
   console.log(sub);
   if (sub == -1) {
-    $scope.techStr += ' ' + tech.Technology + ' ';
+    $scope.techStr += ' ' + tech + ' ';
   } else {
-    var newStr = $scope.techStr.replace(tech.Technology, "");
+    var newStr = $scope.techStr.replace(tech, "");
     console.log(newStr);
     $scope.techStr = newStr;
   }
@@ -19,6 +36,24 @@ $scope.changeTechStr =  function(tech) {
 // $scope.techFilter = function() {
 //   return $scope.Biweekly && $scope.Stuff;
 // };
+
+$scope.location = ['Twin Cities', 'Duluth'];
+
+$scope.typicalSize = ['0-25', '25-50', '50-100', '100-500'];
+
+$scope.changeLocation =  function(location) {
+  console.log('changeLocation hit');
+  console.log(location);
+  var sub = $scope.techStr.indexOf(location);
+  console.log(sub);
+  if (sub == -1) {
+    $scope.techStr += ' ' + location + ' ';
+  } else {
+    var newStr = $scope.techStr.replace(location, "");
+    console.log(newStr);
+    $scope.techStr = newStr;
+  }
+};
 
 groupFactory.getApprovedGroups().then(function(response) {
   $scope.groups = response.data;
@@ -38,65 +73,6 @@ $http({
 }, function myError(response) {
   $scope.tech = response.statusText;
 });//End of http call
-
-$scope.techs = {"Technologies":
-[
-  {
-    "Technology": ".NET",
-    "Skills": [
-      {
-        "Skill": "App Development",
-        "Subskills": ["Windows Communication Foundation",
-          "Windows Forms", "Windows Phone",
-          "Windows Presentation Foundation",
-          "WinRT/Windows 8"]
-      }
-    ]
-    },
-    {
-      "Technology": "CMS",
-      "Skills": [
-        {
-          "Skill": "Ektron",
-          "Subskills": ""
-        },
-        {
-          "Skill": "Kentico",
-          "Subskills": ""
-        },
-        {
-          "Skill": "Sharepoint",
-          "Subskills": ""
-        },
-        {
-          "Skill": "Sitecore",
-          "Subskills": ""
-        },
-        {
-          "Skill": "Sitefinity",
-          "Subskills": ""
-        },
-        {
-          "Skill": "Umbraco",
-          "Subskills": ""
-        }
-      ]
-    },
-    {
-      "Technology": "Frameworks",
-      "Skills": [
-        {
-          "Skill": "ASP.Net",
-          "Subskills": ["ASP.Net MVC", "ASP.Net Web API", "ASP.Net WebForms",
-            "SignalR"]
-        },
-        {
-          "Skill": ["Compact Framework", "Silverlight"]
-        }
-      ]
-    }
-  ]
-};
 
 
 }]) //end homeController
